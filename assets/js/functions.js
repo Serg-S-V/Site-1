291f334dfc652fcf59fa4d46051a607da91550fe
Serg-S-V <skvortsov.s.v1400@gmail.com>
2018-07-30 10:06:02 +0200

    ready

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,6 +3,14 @@
 
   $(document).ready(function () {
 
+  	$('.trigger-menu').click(function() {
+  		$('nav li').slideToggle(500);
+  	});
+  	$(window).resize(function() {
+  		if ($(window).width() > 768){
+  			$('nav li').removeAttr('style');
+  		}
+  	});
 
 
 
